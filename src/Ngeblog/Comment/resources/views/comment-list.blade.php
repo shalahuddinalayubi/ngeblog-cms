e7c097aefb796f3167c9c9006484e33be884d373
unknown <shalahuddinalayubi24@gmail.com>
2022-06-30 09:36:35 +0800

    Update: alayubi/laravel-comment

--- src/Ngeblog/Comment/resources/views/comment-list.blade.php
+++ src/Ngeblog/Comment/resources/views/comment-list.blade.php
@@ -1,12 +1,12 @@
 @foreach ($commentable->comments as $comment)
     <div class="flex items-start">
         <div class="flex items-center justify-center text-center w-10 h-10 rounded-full border-2 mr-3">
-            {{ substr($comment->user->name, 0, 1) }}
+            {{ substr($comment->owner()->name, 0, 1) }}
         </div>
 
         <div class="flex flex-col w-full border-b-2 pb-2">
             <div class="text-xs">
-                <span>{{ $comment->user->name }}</span>
+                <span>{{ $comment->owner()->name }}</span>
                 <span>{{ \Carbon\Carbon::parse($comment->created_at)->diffForHumans() }}</span>
             </div>
 
